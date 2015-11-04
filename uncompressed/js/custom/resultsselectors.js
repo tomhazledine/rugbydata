cecadfc883feee2c0063a8de4d6549f422735272
Tom Hazledine <tom@tomhazledine.com>
2015-11-04 08:58:53 +0000

    js file for checkbox function, and checkobox styling

---
+++ uncompressed/js/custom/resultsselectors.js
@@ -0,0 +1,22 @@
+var selectors = $('.nationSelector');
+
+selectors.on('change',function(){
+	
+	// console.log(this.value);
+
+
+
+	// var checkboxes = searchFilterBox.find('.filterCheckbox');
+	// 		var filters = [];
+	// 		for (var i = 0; i < checkboxes.length; i++) {
+	// 			var checkId = checkboxes[i].id;
+	// 			var checkStatus = checkboxes[i].checked;
+	// 			if (checkStatus) {
+	// 				filters.push(checkId);
+	// 			}
+	// 		}
+
+	// 		filterResultsByType(filters);
+
+	// 		var sleepValue = sleepSelect.val();
+});
