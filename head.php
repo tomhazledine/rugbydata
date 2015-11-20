d9e36c479428393676b73986b62c016c667ae6d3
Tom Hazledine <tom@tomhazledine.com>
2015-11-20 09:01:01 +0000

    typography experiments

--- head.php
+++ head.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
     <title>6 Nations Data</title>
     <link href='https://fonts.googleapis.com/css?family=Lato:400,900,400italic,900italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Abril+Fatface' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="/assets/css/main.css">
 </head>
 
--- story.php
+++ story.php
@@ -1,10 +1,13 @@
 <?php include('header.php'); ?>
 
 <div class="styleTest">
+	<h1>A massive main heading</h1>
 	<div class="textBlock">
 		<h3>A short subheading</h3>
-		<p>Some placeholder paragraph copy to test font size, line height, line length, & margins. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-		<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<div class="columnBlock">
+			<p>Some placeholder paragraph copy to test font size, line height, line length, & margins. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		</div>
 	</div>
 </div>
 
