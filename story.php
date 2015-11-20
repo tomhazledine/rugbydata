6c79b2785fac9958c333b15d66e48634ccd04c85
Tom Hazledine <tom@tomhazledine.com>
2015-11-20 08:36:01 +0000

    added test block for text styling

--- story.php
+++ story.php
@@ -1,5 +1,13 @@
 <?php include('header.php'); ?>
 
+<div class="styleTest">
+	<div class="textBlock">
+		<h3>A short subheading</h3>
+		<p>Some placeholder paragraph copy to test font size, line height, line length, & margins. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	</div>
+</div>
+
 <div class="textBlock">
     <p>The 6 Nations is a yearly rugby tournament held between the top six European national teams: England, Ireland, Wales, Scotland, France, and Italy.</p>
 </div>
