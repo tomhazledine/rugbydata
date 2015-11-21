2e16c8f995e6e04e4b7155f78fcb8bc371dbd4e7
Tom Hazledine <tom@eatenbymonsters.com>
2015-11-21 09:17:29 +0000

    typography experiments

--- story.php
+++ story.php
@@ -3,10 +3,11 @@
 <div class="styleTest">
 	<h1>The 6 Nations: 1992–2015</h1>
 	<div class="textBlock">
-		<h3>A short subheading</h3>
+		<h2>A short subheading</h2>
 		<div class="columnBlock">
-			<p>Some placeholder paragraph copy to test font size, line height, line length, & margins. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <h3>A short subheading</h3>
+			<p>Some placeholder paragraph copy to test font size, <strong>line height</strong>, line length, & margins. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Ut enim ad minim veniam, quis <em>nostrud exercitation ullamco laboris nisi ut aliquip</em> ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
             <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</div>
 	</div>
