99b3a3489515937783e37a832841a65da2b2cdd2
Tom Hazledine <tom@eatenbymonsters.com>
2015-11-21 08:53:39 +0000

    added typekit

--- head.php
+++ head.php
@@ -6,8 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1, minimal-ui">
     <title>6 Nations Data</title>
+
     <link href='https://fonts.googleapis.com/css?family=Lato:400,900,400italic,900italic' rel='stylesheet' type='text/css'>
     <link href='https://fonts.googleapis.com/css?family=Abril+Fatface' rel='stylesheet' type='text/css'>
+
+    <script src="https://use.typekit.net/ypu4urn.js"></script>
+    <script>try{Typekit.load({ async: true });}catch(e){}</script>
+
     <link rel="stylesheet" href="/assets/css/main.css">
 </head>
 
--- story.php
+++ story.php
@@ -1,12 +1,13 @@
 <?php include('header.php'); ?>
 
 <div class="styleTest">
-	<h1>A massive main heading</h1>
+	<h1>The 6 Nations: 1992–2015</h1>
 	<div class="textBlock">
 		<h3>A short subheading</h3>
 		<div class="columnBlock">
-			<p>Some placeholder paragraph copy to test font size, line height, line length, & margins. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-			<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<p>Some placeholder paragraph copy to test font size, line height, line length, & margins. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 		</div>
 	</div>
 </div>
