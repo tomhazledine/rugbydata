836373b0dc4fac1a02ba709c806feb9f97bcee4d
Tom Hazledine <tom@tomhazledine.com>
2015-11-25 08:56:03 +0000

    refactored story intro

--- story.php
+++ story.php
@@ -3,22 +3,17 @@
 <div class="styleTest">
     <div class="ballPoly"></div>
     <!-- <img class="ballPoly_img" src="/assets/images/rugbyballpoly.svg"/> -->
-	<h1>The 6 Nations: 1992–2015</h1>
+	<!-- <h1>The 6 Nations: 1992–2015</h1> -->
 	<div class="textBlock rugbyBlock">
-		<h2>A medium length subheading that spreads to two lines</h2>
+		<h2>The 6 Nations</h2>
 		<div class="columnBlock">
-            <h3>A short subheading</h3>
-			<p>Some placeholder paragraph copy to test font size, <strong>line height</strong>, line length, & margins. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-            <p>Ut enim ad minim veniam, quis <em>nostrud exercitation ullamco laboris nisi ut aliquip</em> ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <h3>Overview</h3>
+            <p>The 6 Nations is a yearly rugby tournament held between the top six European national teams: <em>England</em>, <em>Ireland</em>, <em>Wales</em>, <em>Scotland</em>, <em>France</em>, and <em>Italy</em>.</p>
+			<p>The Six Nations is the successor to the Five Nations Championship which in turn succeeded the Home Nations Championship. The Home Nations Championship was the first international rugby union tournament.</p>
+            <!-- <p>The winners of the Six Nations Championship are sometimes unofficially referred to in the media as the European Champions or Northern Hemisphere Champions. Ireland are the 2015 champions, having finished on equal table points with Wales and England but winning the trophy by virtue of achieving a higher match points difference.</p> -->
+            <!-- <p>Since the Six Nations era started in 2000, only Italy and Scotland have failed to win the Six Nations title, although Scotland were the last outright winners of the Five Nations.</p> -->
 		</div>
 	</div>
-</div>
-
-<div class="textBlock">
-    <div class="columnBlock">
-        <p>The 6 Nations is a yearly rugby tournament held between the top six European national teams: England, Ireland, Wales, Scotland, France, and Italy.</p>
-    </div>
 </div>
 
 <?php include('charts/chart1.php'); ?>
