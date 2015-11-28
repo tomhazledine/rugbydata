53621d92e8ec2164a41c09f24b04b0803df46538
Tom Hazledine <tom@eatenbymonsters.com>
2015-11-28 07:22:40 +0000

    changed pd intro text

--- story.php
+++ story.php
@@ -22,10 +22,7 @@
 	<h2>Points Difference</h2>
 	<div class="columnBlock">
         <h3>Scoring</h3>
-        <p>The 6 Nations is a yearly rugby tournament held between the top six European national teams: <em>England</em>, <em>Ireland</em>, <em>Wales</em>, <em>Scotland</em>, <em>France</em>, and <em>Italy</em>.</p>
-		<p>The Six Nations is the successor to the Five Nations Championship which in turn succeeded the Home Nations Championship. The Home Nations Championship was the first international rugby union tournament.</p>
-        <!-- <p>The winners of the Six Nations Championship are sometimes unofficially referred to in the media as the European Champions or Northern Hemisphere Champions. Ireland are the 2015 champions, having finished on equal table points with Wales and England but winning the trophy by virtue of achieving a higher match points difference.</p> -->
-        <!-- <p>Since the Six Nations era started in 2000, only Italy and Scotland have failed to win the Six Nations title, although Scotland were the last outright winners of the Five Nations.</p> -->
+        <p>The difference between finishing first or second (or even third or fourth) can often be very slight. On occasion, the teams are so well matched that only a handful of scores separate them.</p>
 	</div>
 </div>
 
