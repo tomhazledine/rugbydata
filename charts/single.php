9e26c7111af9ec76a91e431e6342d5c24a554923
Tom Hazledine <tom@tomhazledine.com>
2016-02-02 08:34:53 +0000

    basic single-graph container

---
+++ charts/single.php
@@ -0,0 +1,10 @@
+<section class="singleGraph clearfix">
+    <h2>Single Graph</h2>
+    
+    <div class="singleWrapper clearfix">
+        <div id="single" class="singleGraphContainer"></div>
+    
+        <div class="singleGraphTooltips"></div>
+    </div>
+
+</section>
---
+++ single.php
@@ -0,0 +1,5 @@
+<?php include('header.php'); ?>
+
+<?php include('charts/single.php'); ?>
+
+<?php include('footer.php'); ?>
