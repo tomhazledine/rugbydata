7e0a2dbafa5c8d3d116f4b8089aa6b5241ef3008
Tom Hazledine <tom@tomhazledine.com>
2016-02-25 08:42:12 +0000

    converted tipstricks2 into standard structure for assets

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <?php include('head.php'); ?>
 <header class="mainHeader">
-    <h3 class="mainTitle">The 6 Nations Data Story</h3>
+    <h3 class="mainTitle">Data Vis. experiments</h3>
 </header>
 
 <div class="mainWrapper">
